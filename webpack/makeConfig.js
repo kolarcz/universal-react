27aac6210d53941bd79a0b641128b3f4d8492b40
Petr Kolář <kolarcz@gmail.com>
2017-04-20 16:44:44 +0200

    webpack 2

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -11,11 +11,12 @@ module.exports = (ENV) => {
     .development(isDEV);
 
   return {
-    devtool: isDEV ? 'cheap-module-eval-source-map' : null,
+    devtool: isDEV ? 'cheap-module-eval-source-map' : false,
     entry: (isDEV ? ['webpack-hot-middleware/client', 'react-hot-loader/patch'] : [])
       .concat([
         `${__dirname}/../src/client/index.js`
       ]),
+    context: `${__dirname}/../src`,
     output: {
       filename: '[hash:8].js',
       chunkFilename: '[hash:8].[id].js',
@@ -26,73 +27,99 @@ module.exports = (ENV) => {
       new ProgressBarPlugin({
         format: 'Build :percent   :msg'
       }),
+      new webpack.NoEmitOnErrorsPlugin(),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(ENV),
         __CLIENT__: true,
         __DEV__: isDEV
       }),
-      new webpack.optimize.OccurenceOrderPlugin()
+      new webpack.LoaderOptionsPlugin({
+        minimize: !isDEV,
+        debug: isDEV,
+        options: {
+          postcss: [
+            autoprefixer({
+              browsers: ['last 2 versions']
+            })
+          ]
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        comments: isDEV
+      }),
+      new ExtractTextPlugin({
+        filename: '[hash:8].css',
+        allChunks: true
+      })
     ].concat(isDEV ? [
       new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
-    ] : [
-      new ExtractTextPlugin('[hash:8].css', { allChunks: true }),
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({ compress: { screw_ie8: true, warnings: false } })
-    ]).concat(
+      new webpack.NamedModulesPlugin()
+    ] : []).concat(
       webpackIsomorphicToolsPlugin
     ),
     resolve: {
-      extensions: ['', '.js', '.jsx']
+      extensions: ['.js', '.jsx'],
+      modules: [
+        `${__dirname}/../src`,
+        'node_modules'
+      ]
     },
     module: {
-      loaders: [{
+      rules: [{
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-          plugins: [
-            ['transform-runtime', { polyfill: false, regenerator: true }],
-            'transform-object-rest-spread',
-            'transform-async-to-generator'
-          ].concat(isDEV ? 'react-hot-loader/babel' : [])
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'react',
+              ['env', { modules: false }]
+            ],
+            plugins: [
+              'transform-object-rest-spread',
+              'transform-class-properties'
+            ].concat(isDEV ? 'react-hot-loader/babel' : [])
+          }
         }
       }, {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: isDEV
-          ? 'style-loader!css-loader?modules&sourceMap&localIdentName=[hash:base64:6]!postcss-loader'
-          : ExtractTextPlugin.extract('style-loader', 'css-loader?modules&localIdentName=[hash:base64:6]!postcss-loader')
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [`css-loader?modules${isDEV ? '&sourceMap' : ''}&localIdentName=[hash:base64:8]`, 'postcss-loader']
+        })
       }, {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loader: isDEV
-          ? 'style-loader!css-loader?modules&sourceMap&localIdentName=[hash:base64:6]!postcss-loader!sass-loader'
-          : ExtractTextPlugin.extract('style-loader', 'css-loader?modules&localIdentName=[hash:base64:6]!postcss-loader!sass-loader')
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [`css-loader?modules${isDEV ? '&sourceMap' : ''}&localIdentName=[hash:base64:8]`, 'postcss-loader', 'sass-loader']
+        })
       }, {
         test: /\.css$/,
         include: /node_modules/,
-        loader: isDEV
-          ? 'style-loader!css-loader'
-          : ExtractTextPlugin.extract('style-loader', 'css-loader')
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        })
       }, {
         test: /\.scss$/,
         include: /node_modules/,
-        loader: isDEV
-          ? 'style-loader!css-loader!sass-loader'
-          : ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       }, {
         test: /\.(eot|ttf|woff2?)(\?.*)?$/,
-        loader: 'url-loader?name=[hash:8].[ext]&limit=1'
+        use: 'url-loader?name=[hash:8].[ext]&limit=1'
       }, {
         test: /\.(gif|jpe?g|png|svg)(\?.*)?$/,
-        loader: 'url-loader?name=[hash:8].[ext]&limit=10000'
+        use: 'url-loader?name=[hash:8].[ext]&limit=10000'
       }, {
         test: /\.ico(\?.*)?$/,
-        loader: 'url-loader?name=[hash:8].[ext]&limit=1'
+        use: 'url-loader?name=[hash:8].[ext]&limit=1'
       }]
-    },
-    postcss: [autoprefixer({ browsers: ['last 2 version'] })],
+    }
   };
 };
